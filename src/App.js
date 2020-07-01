ad5d30fdef37c7cbb15cfb236c315c0200da3c3a
gabriel-dasilvasousa <g.silva13455@gmail.com>
2020-06-30 21:13:01 -0300

    Version 1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import Routes from './routes'
 import './styles.css'
 
 import Header from './components/Header'
@@ -9,7 +10,7 @@ import Main from './pages/main';
 const App = () => (
   <div className="App">
     <Header /> 
-    <Main />   
+    <Routes />   
   </div>
 
 );
